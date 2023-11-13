ad659464bcf3755d656a054aff651bd2884f27da
leconte <leconteyohan15@gmail.com>
2023-11-14 00:57:56 +0100

    cammand_parser.c version 1 pwd

---
+++ command_parser.c
@@ -0,0 +1,13 @@
+
+#include <errno.h>
+
+int V_return;
+
+void pwd(){
+    char *buf;
+    getcwd(buf);
+    if(errno==ERANGE){V_return=1;return 1;}
+    printf(buf);
+    V_return=0;
+    return 0;
+}
